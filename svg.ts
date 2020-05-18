777c50842589ec0d7221e1e86b4dda91164a0e85
noviodelangel <mateuszwerpulewski@gmail.com>
2020-05-19 01:30:07 +0200

    Add 3 based hex support Add named colors support

--- svg.ts
+++ svg.ts
@@ -1,26 +1,55 @@
 import * as grunt from 'grunt';
 import * as path from 'path';
+import * as util from 'util';
+import * as SVG from '@svgdotjs/svg.js';
+import {ColorLike} from "@svgdotjs/svg.js";
 
-const sources = grunt.file.expand('svg**/*.svg');
+const sources = grunt.file.expand('svg/**/*.svg');
+const namedColors: Array<string> = grunt.file.read('named-colors.txt').split('\n');
+const selectedNamedColors: Map<string, string> = new Map([
+    ['black', '#000000'],
+    ['gold', '#FFD700'],
+    ['gray', '#808080'],
+    ['green', '#008000'],
+    ['orange', '#FFA500'],
+    ['purple', '#800080'],
+    ['red', '#FF0000'],
+    ['silver', '#C0C0C0'],
+    ['white', '#FFFFFF'],
+]);
 
-function getGrayscaleColor(fill: string) {
-    if (fill.startsWith('#') && fill.length === 7) {
-        return eightBitRGBToGrayscale(fill);
+/**
+ * Converts given fill color into grayscale
+ * @param fill - input color
+ * @param fileName - file name for logging purposes (in case we want to know which files contain certain color encodings)
+ */
+function getGrayscaleColor(fill: string, fileName?: string) {
+    if (fill.startsWith('#')) {
+        // if (fileName && fill.length == 4) console.log(`3-based-color=${fill} file=${fileName}`);
+        return hexStringToGrayscale(fill);
+    }
+    if (namedColors.includes(fill)) {
+        if (!selectedNamedColors.get(fill)) {
+            console.log(`Unknown color mapping for: ${fill}`);
+            return fill;
+        }
+        return hexStringToGrayscale(selectedNamedColors.get(fill));
     }
     return fill;
 }
 
-function eightBitRGBToGrayscale(fill: string) {
-    fill = fill.substr(1);
-    const R = parseInt(`0x${fill.substr(0, 2)}`, 16);
-    const G = parseInt(`0x${fill.substr(2, 2)}`, 16);
-    const B = parseInt(`0x${fill.substr(4, 2)}`, 16);
-    const grayScale = Math.round((0.3 * R) + (0.59 * G) + (0.11 * B));
+/**
+ * @param fill - accepts both three based (e.g. #f06) and six based (e.g. #ff0066) hex format
+ */
+function hexStringToGrayscale(fill: string) {
+    const color: ColorLike = new SVG.Color(fill);
+    const grayScale = Math.round((0.3 * color.r) + (0.59 * color.g) + (0.11 * color.b));
     const grayScaleHex = grayScale.toString(16);
     return `#${grayScaleHex}${grayScaleHex}${grayScaleHex}`;
 }
 
 sources.forEach(filePath => {
+    //console.log(util.inspect(namedColors, { maxArrayLength: null }));
     const fileName: string = path.basename(filePath);
     const svgFileContent: string = grunt.file.read(filePath);
     const fillRegExp: RegExp = new RegExp('fill="(.*?)"', 'g');
@@ -29,13 +58,13 @@ sources.forEach(filePath => {
 
     let fillMatch: RegExpExecArray = null;
     while(fillMatch = fillRegExp.exec(output)) {
-        const newFill: string = getGrayscaleColor(fillMatch[1]);
+        const newFill: string = getGrayscaleColor(fillMatch[1], fileName);
         output = output.replace(`fill="${fillMatch[1]}"`, `fill="${newFill}"`);
     }
 
     let gradientMatch: RegExpExecArray = null;
     while(gradientMatch = gradientRegExp.exec(output)) {
-        const newGradient: string = getGrayscaleColor(gradientMatch[1]);
+        const newGradient: string = getGrayscaleColor(gradientMatch[1], fileName);
         output = output.replace(`stop-color="${gradientMatch[1]}"`, `stop-color="${newGradient}"`);
     }
 
