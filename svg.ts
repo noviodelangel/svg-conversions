d33377e1e4d28d9b6954f96b6687dd5cbb4010e0
noviodelangel <mateuszwerpulewski@gmail.com>
2020-05-21 00:14:25 +0200

    Preserving output directory structure Styles to grayscale

--- svg.ts
+++ svg.ts
@@ -2,9 +2,11 @@ import * as grunt from 'grunt';
 import * as path from 'path';
 import * as util from 'util';
 import * as SVG from '@svgdotjs/svg.js';
-import {ColorLike} from "@svgdotjs/svg.js";
+import {Color} from "@svgdotjs/svg.js";
 
-const sources = grunt.file.expand('svg/**/*.svg');
+const inputFolder = 'svg';
+const outputFolder = 'out';
+const sources = grunt.file.expand(`${inputFolder}/**/*.svg`);
 const namedColors: Array<string> = grunt.file.read('named-colors.txt').split('\n');
 const selectedNamedColors: Map<string, string> = new Map([
     ['black', '#000000'],
@@ -26,14 +28,13 @@ const selectedNamedColors: Map<string, string> = new Map([
 function getGrayscaleColor(fill: string, fileName?: string) {
     if (fill.startsWith('#')) {
         // if (fileName && fill.length == 4) console.log(`3-based-color=${fill} file=${fileName}`);
-        return hexStringToGrayscale(fill);
+        return rgbStringToGrayscale(fill);
+    }
+    if (fill.startsWith('rgb(')) {
+        return rgbStringToGrayscale(fill);
     }
     if (namedColors.includes(fill)) {
-        if (!selectedNamedColors.get(fill)) {
-            console.log(`Unknown color mapping for: ${fill}`);
-            return fill;
-        }
-        return hexStringToGrayscale(selectedNamedColors.get(fill));
+        return namedColorToGrayscale(fill);
     }
     return fill;
 }
@@ -41,32 +42,73 @@ function getGrayscaleColor(fill: string, fileName?: string) {
 /**
  * @param fill - accepts both three based (e.g. #f06) and six based (e.g. #ff0066) hex format
  */
-function hexStringToGrayscale(fill: string) {
-    const color: ColorLike = new SVG.Color(fill);
+function rgbStringToGrayscale(fill: string) {
+    const color: Color = new SVG.Color(fill);
     const grayScale = Math.round((0.3 * color.r) + (0.59 * color.g) + (0.11 * color.b));
-    const grayScaleHex = grayScale.toString(16);
-    return `#${grayScaleHex}${grayScaleHex}${grayScaleHex}`;
+    const grayScaleColor: Color = new SVG.Color(grayScale, grayScale, grayScale, 'rgb');
+    return grayScaleColor.toHex();
 }
+
+function namedColorToGrayscale(fill: string) {
+    if (!selectedNamedColors.get(fill)) {
+        console.error(`Unknown color mapping for: ${fill}`);
+        return fill;
+    }
+    return rgbStringToGrayscale(selectedNamedColors.get(fill));
+}
+
+function processFill(output: string, fileName: string) {
+    const fillRegExp: RegExp = new RegExp('fill="(.*?)"', 'g');
+    let fillMatch: RegExpExecArray = null;
+    while (fillMatch = fillRegExp.exec(output)) {
+        const newFill: string = getGrayscaleColor(fillMatch[1], fileName);
+        output = output.replace(`fill="${fillMatch[1]}"`, `fill="${newFill}"`);
+    }
+    return output;
+}
+
+function processGradient(output: string, fileName: string) {
+    const gradientRegExp: RegExp = new RegExp('stop-color="(.*?)"', 'g');
+    let gradientMatch: RegExpExecArray = null;
+    while (gradientMatch = gradientRegExp.exec(output)) {
+        const newGradient: string = getGrayscaleColor(gradientMatch[1], fileName);
+        output = output.replace(`stop-color="${gradientMatch[1]}"`, `stop-color="${newGradient}"`);
+    }
+    return output;
+}
+
+function processStyles(output: string, fileName: string) {
+    const styleRegExp: RegExp = new RegExp('fill:\\s*(.*?);', 'g');
+    let styleMatch: RegExpExecArray = null;
+    while (styleMatch = styleRegExp.exec(output)) {
+        const newStyle: string = getGrayscaleColor(styleMatch[1], fileName);
+        // output = output.replace(new RegExp(`fill:\\s*${styleMatch[1]}`), `fill:${newStyle}`); -> this was not working for codelyzer.svg, not sure why. Applying following workaround:
+        output = output.replace(`fill: ${styleMatch[1]}`, `fill:${newStyle}`);
+        output = output.replace(`fill:${styleMatch[1]}`, `fill:${newStyle}`);
+    }
+    return output;
+}
+
+function getRelativeFolderPath(filePath) {
+    let relativeOutputPath: string = '';
+    const pathArray = filePath.split(path.sep);
+    for (let i = 1; i < pathArray.length - 1; i++) {
+        relativeOutputPath = relativeOutputPath + path.sep + pathArray[i];
+    }
+    return relativeOutputPath;
+}
+
 
 sources.forEach(filePath => {
     //console.log(util.inspect(namedColors, { maxArrayLength: null }));
     const fileName: string = path.basename(filePath);
+    const fileFolder: string = getRelativeFolderPath(filePath);
     const svgFileContent: string = grunt.file.read(filePath);
-    const fillRegExp: RegExp = new RegExp('fill="(.*?)"', 'g');
-    const gradientRegExp: RegExp = new RegExp('stop-color="(.*?)"', 'g');
     let output: string = svgFileContent;
 
-    let fillMatch: RegExpExecArray = null;
-    while(fillMatch = fillRegExp.exec(output)) {
-        const newFill: string = getGrayscaleColor(fillMatch[1], fileName);
-        output = output.replace(`fill="${fillMatch[1]}"`, `fill="${newFill}"`);
-    }
+    output = processFill(output, fileName);
+    output = processGradient(output, fileName);
+    output = processStyles(output, fileName);
 
-    let gradientMatch: RegExpExecArray = null;
-    while(gradientMatch = gradientRegExp.exec(output)) {
-        const newGradient: string = getGrayscaleColor(gradientMatch[1], fileName);
-        output = output.replace(`stop-color="${gradientMatch[1]}"`, `stop-color="${newGradient}"`);
-    }
-
-    grunt.file.write(`out/${fileName}`, output);
+    grunt.file.write(`${outputFolder}${fileFolder}/${fileName}`, output);
 });
