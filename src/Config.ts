0a67f0526cb6f9ba405d8ca6761c37bfd9071b06
Karol Depka <karol-depka@users.noreply.github.com>
2022-11-02 23:11:55 +0100

    karol config fix .../logos path

--- src/Config.ts
+++ src/Config.ts
@@ -2,10 +2,12 @@ export class Config {
     constructor(
         public inputFolder: string = '../../InnoTopic_Website/InnoTopicWebsite/src/assets/images/logos',
         // public inputFolder: string = 'example_data/single_color',
-        public outputFolder: string = '../../InnoTopic_Website/InnoTopicWebsite/src/assets/images/logos-l/',
+        public outputFolder: string = '../../InnoTopic_Website/InnoTopicWebsite/src/assets/images/logos-l/logos',
         // public outputFolder: string = '../out/example_data/single_color',
-        public primaryColor: string = '#00acc1',
-        public tolerance: number = 0.6,
+        // public primaryColor: string = '#00acc1',
+        public primaryColor: string = '#ff9d4e',
+        // public tolerance: number = 0.6,
+        public tolerance: number = 0.3,
         public outputMode: string = 'rgb'
     ) {
     }
