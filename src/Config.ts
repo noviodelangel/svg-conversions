06a811a41db97068dc08f347e28e387b2f46138f
Karol Depka <karol-depka@users.noreply.github.com>
2023-04-24 20:01:39 +0200

    Shirt prepared for print; darker gray icons and darker logo

--- src/Config.ts
+++ src/Config.ts
@@ -5,10 +5,20 @@ export class Config {
         public outputFolder: string = '../../InnoTopic_Website/InnoTopicWebsite/src/assets/images/logos-l/logos',
         // public outputFolder: string = '../out/example_data/single_color',
         // public primaryColor: string = '#00acc1',
-        public primaryColor: string = '#ff9d4e',
+        // public primaryColor: string = '#ff9d4e',
+
+        // public primaryColor: string = '#77743b',
+        // public primaryColor: string = '#808080',
+        public primaryColor: string = '#404040',
+        // public primaryColor: string = '#505050',
+
         // public tolerance: number = 0.6,
-        public tolerance: number = 0.3,
+        // public tolerance: number = 0.3,
+        // public tolerance: number = 0.35,
+        public tolerance: number = 0.40,
         public outputMode: string = 'rgb'
+
+        // TODO separate preset for print and for web
     ) {
     }
 }
